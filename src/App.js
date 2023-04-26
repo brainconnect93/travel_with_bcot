006f9034ad66ecac145e9da6b27150b047704d43
brainconnect93 <brainconnect0@gmail.com>
2023-04-26 12:34:24 +0100

    Install some dependencies for the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
-	return (
-		<div className="App">
-		  <h1>Hello World I am here</h1>
-		</div>
-	);
+const App = () => {
+  return (
+	<div>
+	  <h1>Hello World I am Here</h1>
+	</div>
+  )
 }
 
-export default App;
+export default App
