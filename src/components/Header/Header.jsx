cf5f5c9870442423a54023d4e21ec40d47f2cff6
brainconnect93 <brainconnect0@gmail.com>
2023-07-31 07:09:05 +0100

    Working with Navbar heading

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from "@material-ui/icons/Search";
 
-const Header = () => (
-  <div>
-    <h1>Header</h1>
-  </div>
-);
+
+const Header = () => {
+  return (
+    <>
+      <AppBar position='static'>
+        <Toolbar className={classes.toolbar}>
+          <Typography variant='h4' className={classes.title}>
+            Travel with Bcot
+          </Typography>
+
+          <Box display='flex'>
+            <Typography variant='h6' className={classes.title}>
+              Explore new places
+            </Typography>
+            <Autocomplete>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <SearchIcon />
+                </div>
+                <InputBase placeholder='Search...' classes={{ root: classes.inputRoot, input: classes.inputImput }} />
+              </div>
+            </Autocomplete>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
 export default Header;
